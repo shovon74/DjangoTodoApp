133862cf54c5baf2911e6294c7d98c22503997bb
Shib Shankar Bose <shibshankarbose71@gmail.com>
2019-03-21 16:35:38 +0600

    create models

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    task_name = models.CharField(max_length=200)
+    task_description = models.CharField(max_length=200)
+    task_priority = models.CharField(max_length=50)
